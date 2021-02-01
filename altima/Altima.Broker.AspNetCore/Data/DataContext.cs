715835408584674c7bab771ced69fdd2632894ce
Cristiano Alves <cristiano.alves@linx.com.br>
2021-02-01 09:11:34 -0300

    Register Models

--- altima/Altima.Broker.AspNetCore/Data/DataContext.cs
+++ altima/Altima.Broker.AspNetCore/Data/DataContext.cs
@@ -1,6 +1,10 @@
-﻿using System.Reflection;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
+using Altima.Broker.Business;
 using Microsoft.EntityFrameworkCore;
-using Officedriver.Contratos.Core.Cooperados.Model;
 
 namespace Altima.Broker.AspNet.Mvc.Data
 {
@@ -8,17 +12,32 @@ namespace Altima.Broker.AspNet.Mvc.Data
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
+            //
         }
 
-        public DbSet<Cooperado> Cooperados { get; set; }
+  
+
+        //public DbSet<Cooperado> Cooperados { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            RegisterAllModels(builder);
             base.OnModelCreating(builder);
             builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
 
+        private void RegisterAllModels(ModelBuilder modelBuilder)
+        {
+            var referencedPaths = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.dll").ToList<string>();
+            foreach (var reference in referencedPaths)
+            {
+                var currentAssembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(reference);
 
-
+                foreach (var entity in currentAssembly.GetExportedTypes().Where(x => typeof(BaseModel).IsAssignableFrom(x) && !x.IsAbstract))
+                {
+                    modelBuilder.Entity(entity);
+                };
+            }
         }
     }
 }
