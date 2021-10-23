585f67b42765d660a16f62ec3ccc84f3396c35e8
Flávio Oliveita <flavio.oliveira@millennium.com.br>
2021-10-23 18:54:49 -0300

    Conclusao Apontamentos.xml Inicio processo de fechamento

---
+++ Module/services-dll/Officedriver.dpr
@@ -0,0 +1,13 @@
+library Officedriver;
+
+
+uses
+  wtsmemorym_intf in 'z:\wdts\server\common\wtsmemorym_intf.pas',
+  wtsServerObjs in 'Z:\WDTS\server\common\wtsServerObjs.pas',
+  officedriver_contratos_fechamentos in 'officedriver_contratos_fechamentos.pas';
+
+exports
+    wtsLibEntry,
+    wtsLibShutdown;
+begin
+end.
---
+++ Module/services-dll/officedriver_contratos_fechamentos.pas
@@ -0,0 +1,68 @@
+unit officedriver_contratos_fechamentos;
+
+interface
+
+uses
+  wtsServerObjs;
+
+implementation
+
+procedure FecharContrato(Input:IwtsInput; Output:IwtsOutput;DataPool:IwtsDataPool);
+var
+   C: IwtsCommand;
+   ApontamentosCalculados: IwtsData;
+begin
+  C := DataPool.Open('OFFICEDRIVER');
+  C.Execute('#CALL OFFICEDRIVER.CONTRATOS.FECHAMENTOS.CALCULARCONTRATO(:Fechamento,:Contrato)');
+  ApontamentosCalculados := C.CreateRecordset;
+  while not ApontamentosCalculados.EOF do
+  begin
+     C.Dim('Fechamento',Input.Value['Fechamento']);
+     C.Dim('Contrato',Input.Value['Contrato']);
+     C.Dim('Apontamento', ApontamentosCalculados.Value['Apontamento']);
+     C.Dim('ValorRepasse', ApontamentosCalculados.Value['ValorRepasse']);
+     C.Dim('ValorCobranca', ApontamentosCalculados.Value['ValorCobranca']);
+     C.Execute('INSERT INTO FechamentosApontamentos(Fechamento,Contrato,Apontamento,ValorRepasse,ValorCobranca) VALUES (:Fechamento,:Contrato,:Apontamento,:ValorRepasse,:ValorCobranca) #RETURN(ID)');
+
+     ApontamentosCalculados.Next;
+  end;
+end;
+
+procedure CalcularContrato(Input:IwtsInput; Output:IwtsOutput;DataPool:IwtsDataPool);
+var
+   C: IwtsCommand;
+   Fechamento,Contrato,Apontamentos: IwtsData;
+begin
+  C := DataPool.Open('OFFICEDRIVER');
+
+  C.Dim('Fechamento', Input.Value['Fechamento']);
+  C.Execute('SELECT * FROM Fechamentos WHERE Id =:Fechamento');
+  Fechamento := C.CreateRecordset;
+
+  C.Dim('Contrato', Input.Value['Contraro']);
+  C.Execute('SELECT * FROM Contratos WHERE Contrato =:Contrato');
+  Contrato := C.CreateRecordset;
+
+
+  C.Dim('Contrato', Input.Value['Contrato']);
+  C.Dim('DataBaseFechamento', Fechamento.Value['DataBaseFechamento']);
+  C.Execute('SELECT * FROM Apontamentos WHERE Contrato =:Contrato AND Fechamento IS NULL and DataEntrada <= :DataBaseFechamento');
+  Apontamentos := C.CreateRecordset;
+
+  while not Apontamentos.EOF do
+  begin
+   // Apontamento
+  //  ValorCobranca
+  //  ValorRepasse
+    Apontamentos.Next;
+  end;
+
+end;
+
+
+
+initialization
+  wtsRegisterProc('CONTRATOS.FECHAMENTOS.FecharContrato', FecharContrato);
+  wtsRegisterProc('CONTRATOS.FECHAMENTOS.CalcularContrato', CalcularContrato);
+
+end.
